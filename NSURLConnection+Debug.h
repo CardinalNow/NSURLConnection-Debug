66fecf18f058c6e77145d29a02f083f8931d83e7
Rusty Weneck <rweneck@cardinalsolutions.com>
2013-03-15 13:45:14 -0400

    Added NSURLConnection+Debug

---
+++ NSURLConnection+Debug.h
@@ -0,0 +1,13 @@
+//
+//  NSURLConnection+Debug.h
+//  Vantiv
+//
+//  Created by Rusty Weneck on 3/15/13.
+//  Copyright (c) 2013 Cardinal Solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSURLConnection (Debug)
+
+@end
